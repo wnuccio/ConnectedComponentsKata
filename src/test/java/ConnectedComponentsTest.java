a1be94adcb885944754e9aafa3179bf00e85f4af
Walter Nuccio <wnuccio@hotmail.com>
2020-08-17 19:54:14 +0200

    Initial commit

---
+++ src/test/java/ConnectedComponentsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ConnectedComponentsTest {
+
+    @Test
+    public void return_one_for_one_character_string() {
+        fail();
+    }
+
+}
