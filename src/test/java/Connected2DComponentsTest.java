7300e486db39359747296b7d869410da52b65501
Walter Nuccio <wnuccio@hotmail.com>
2020-08-19 17:49:56 +0200

    test simple cases with only two symbols

--- src/test/java/Connected2DComponentsTest.java
+++ src/test/java/Connected2DComponentsTest.java
@@ -4,10 +4,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Connected2DComponentsTest {
     @Test
-    void return_one_for_one_char() {
-        assertEquals(1, new Connected2DComponents(
-                "OO",
-                "OO"
-        ).componentsNumber());
+    void return_one_for_one_symbol() {
+        assertEquals(1, new Connected2DComponents(new int[][]{
+                {0, 0},
+                {0, 0}
+        }).componentsNumber());
     }
+
+    @Test
+    void return_two_for_two_symbols() {
+        assertEquals(2, new Connected2DComponents(new int[][]{
+                {0, 0},
+                {0, 1}
+        }).componentsNumber());
+    }
+
+//    @Test
+//    void return_four_with_two_symbols_all_separated() {
+//        assertEquals(4, new Connected2DComponents(new int[][]{
+//                {1, 0},
+//                {0, 1}
+//        }).componentsNumber());
+//    }
 }
