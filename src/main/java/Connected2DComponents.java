8736479895d533f23d4b4d409c3e6f3912583dea
Walter Nuccio <wnuccio@hotmail.com>
2020-08-20 12:30:11 +0200

    refactor to clean algorithm

--- src/main/java/Connected2DComponents.java
+++ src/main/java/Connected2DComponents.java
@@ -1,6 +1,6 @@
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 
 public class Connected2DComponents {
     private final int[][] grid;
@@ -13,41 +13,53 @@ public class Connected2DComponents {
         this.jMax = grid[0].length - 1;
     }
 
-    private void findConnectionAround(Point point, Connection currentConnection, List<Connection> foundConnections) {
+    private void extendConnectionInAllDirections(Point point, Connection currentConnection, List<Connection> foundConnections) {
         for (Direction d: Direction.values()) {
-            connectionExtension(point, d, currentConnection, foundConnections);
+            extendConnection(currentConnection, point, d, foundConnections);
         }
     }
 
-    private void connectionExtension(Point point, Direction d, Connection currentConnection, List<Connection> foundConnections) {
-        Point adjacentPoint = null;
-
-        switch (d) {
-            case UP:
-                adjacentPoint = point.i != 0 ? new Point(point.i-1, point.j, point.symbol) : null;
-                break;
-            case DOWN:
-                adjacentPoint = point.i != iMax ? new Point(point.i+1, point.j, point.symbol) : null;
-                break;
-            case RIGHT:
-                adjacentPoint = point.j != 0 ? new Point(point.i, point.j-1, point.symbol) : null;
-                break;
-            case LEFT:
-                adjacentPoint = point.j != jMax ? new Point(point.i, point.j+1, point.symbol) : null;
-                break;
-        }
+    private void extendConnection(Connection currentConnection, Point currentPoint, Direction d, List<Connection> foundConnections) {
+        Point adjacentPoint = adjacentPoint(currentPoint, d).orElse(null);
 
         if (adjacentPoint == null) return;
-
         if (currentConnection.contains(adjacentPoint)) return;
-
-        Point finalAdjacentPoint = adjacentPoint;
-        if (foundConnections.stream().anyMatch(conn -> conn.contains(finalAdjacentPoint))) return;
-
-        if (point.symbol != grid[adjacentPoint.i][adjacentPoint.j]) return;
+        if (foundConnections.stream().anyMatch(conn -> conn.contains(adjacentPoint))) return;
+        if (currentPoint.symbol != adjacentPoint.symbol) return;
 
         currentConnection.add(adjacentPoint);
-        findConnectionAround(adjacentPoint, currentConnection, foundConnections);
+        extendConnectionInAllDirections(adjacentPoint, currentConnection, foundConnections);
+    }
+
+    private Optional<Point> adjacentPoint(Point point, Direction d) {
+        int i, j;
+        switch (d) {
+            case UP:
+                i = point.i-1;
+                j = point.j;
+                break;
+            case DOWN:
+                i = point.i+1;
+                j = point.j;
+                break;
+            case RIGHT:
+                i = point.i;
+                j = point.j-1;
+                break;
+            case LEFT:
+                i = point.i;
+                j = point.j+1;
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid direction: " + d);
+        }
+
+        Point adjacentPoint = areValid(i, j) ? new Point(i, j, symbolAt(i, j)) : null;
+        return Optional.ofNullable(adjacentPoint);
+    }
+
+    private boolean areValid(int i, int j) {
+        return (i >= 0 && i<=iMax && j>=0 && j<=jMax);
     }
 
     public int componentsNumber() {
@@ -55,16 +67,21 @@ public class Connected2DComponents {
 
         for (int i = 0; i < grid.length; ++i) {
             for (int j = 0; j < grid[i].length; j++) {
-                Point currentPoint = new Point(i, j, grid[i][j]);
+                int symbol = symbolAt(i, j);
+                Point currentPoint = new Point(i, j, symbol);
                 if (!inAnyConnection(currentPoint, foundConnections)) {
                     Connection newConnection = Connection.empty();
                     newConnection.add(currentPoint);
-                    findConnectionAround(currentPoint, newConnection, foundConnections);
+                    extendConnectionInAllDirections(currentPoint, newConnection, foundConnections);
                     foundConnections.add(newConnection);
                 }
             }
         }
         return foundConnections.size();
+    }
+
+    private int symbolAt(int i, int j) {
+        return grid[i][j];
     }
 
     private boolean inAnyConnection(Point currentPoint, List<Connection> allConnections) {
